22ecf4c3d18ad303cd458f433bc2e1ca695cec4b
david-rollinson <davrc.me@gmail.com>
2022-11-01 22:23:45 +0000

    debugging

--- portfolio/src/components/Maintenance.vue
+++ portfolio/src/components/Maintenance.vue
@@ -3,8 +3,10 @@ import MaintenanceItem from "./MaintenanceItems.vue";
 </script>
 
 <template>
+   <header>
  <MaintenanceItem>
     <template #heading>Undergoing Maintenance, please come back again later.
     </template>
  </MaintenanceItem>
+   </header>
 </template>
