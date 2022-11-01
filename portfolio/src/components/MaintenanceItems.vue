47acbaf937821a8f6bb568aa7233660f897a25f9
david-rollinson <davrc.me@gmail.com>
2022-11-01 23:10:36 +0000

    added fonts for mobile distribution

--- portfolio/src/components/MaintenanceItems.vue
+++ portfolio/src/components/MaintenanceItems.vue
@@ -1,15 +1,31 @@
 <template>
     <!-- this should probably eventually be refactored into a new file? -->
       <div class="maintenanceMsg">
-        <h2>
+        <h1>
           <slot name="heading"></slot>
           <!-- //the maintenance message should be dropped in here somehow  -->
-        </h2>
+        </h1>
         <slot></slot>
         </div>
 </template>
   
   <style scoped>
+
+  @font-face {
+    font-family: 'Codystar', sans-serif;
+    src: local(Codystar),
+    url(./fonts/Codystar.ttf);
+  }
+  @font-face {
+    font-family: 'Montserrat Alternates', sans-serif;
+    src: local(Montserrat),
+    url(./fonts/Montserrat.ttf);
+  }
+  @font-face {
+    font-family: 'Assistant', sans-serif;
+    src: local(Assistant),
+    url(./fonts/Assistant.ttf);
+  }
   .maintenanceMsg {
     margin-top: 50%;
     height: 100%;
@@ -24,8 +40,8 @@
     margin-left: 1rem;
   } */
   
-  h2 {
-    font-family: Montserrat Alternates;
+  h1 {
+    font-family: Codystar;
     font-size: 1.2rem;
     font-weight: 500;
     /* margin-bottom: 0.4rem; */
