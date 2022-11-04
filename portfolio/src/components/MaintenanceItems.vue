bdd8a34c62506cc73a880b1c2582400d212c0abd
david-rollinson <davrc.me@gmail.com>
2022-11-04 22:24:24 +0000

    completed maintenance screen

--- portfolio/src/components/MaintenanceItems.vue
+++ portfolio/src/components/MaintenanceItems.vue
@@ -1,17 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const show = ref(false)
+</script>
+
 <template>
     <!-- this should probably eventually be refactored into a new file? -->
       <div class="maintenanceMsg" draggable="true">
-        <h1>
+        <h1 @mouseover="show = true">
           <slot name="heading"></slot>
           <!-- //the maintenance message should be dropped in here somehow  -->
         <slot></slot>
         </h1>
+      </div>
+      <div class="bounceIn">
+          <Transition name="bounce">
+             <p v-if="show">In the meantime you can contact me at @devushkabeam</p>
+          </Transition>
         </div>
 </template>
-  
+
   <style scoped>
   .maintenanceMsg {
-    height: 100vh;
+    height: 90vh;
     vertical-align: middle;
     display: flex;
     background-color: transparent;
@@ -19,22 +30,52 @@
     align-items: center;
     text-align: center;
   }
-  
-  /* .details {
-    flex: 1;
-    margin-left: 1rem;
-  } */
+
+  .bounceIn {
+    z-index: 1;
+    display: flex;
+    background-color: yellow;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
   
   h1 {
     font-family: Codystar;
     font-size: 26px;
     font-weight: 800;
-    color: black;
+    color: blue;
     position:absolute;
   }
 
   h1:hover {
     cursor:cell;
+    background-color: yellow;
+    width:fit-content;
   }
+
+  p {
+    font-family: Assistant;
+    background-color: yellow;
+    font-size: 16px;
+    font-weight: 100;
+    color: blue;
+    position:absolute;
+  }
+
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  60% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
 
   </style>
