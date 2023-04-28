e189f82c588adb95ba4516361492e3279b131964
david-rollinson <davrc.me@gmail.com>
2023-04-29 00:23:18 +0100

    optimised work page for portrait devices

--- portfolio/src/views/Work.vue
+++ portfolio/src/views/Work.vue
@@ -41,7 +41,7 @@
   </header>
 
   <body>
-    <div class="container">
+    <div class="grid_container">
     <div class="projects">
         <RouterLink class="grid_item" style="cursor:cell" v-for="project in projects"
         :key="project.id" 
@@ -51,9 +51,13 @@
         @mouseleave="mouseLeave"
         > <!--go to the project.show route as set in index.js-->
         <!-- <h2>{{ project.name }}</h2> -->
+        <div class="image_border">
+        <div class="image_container">
         <img :src="project.image" class="content_img" ref="content_ref"/>
+        </div>
+        </div>
         <!-- template including image file, and brief description preview? -->
-        <!-- <h1>{{ project.description }}</h1> -->
+        <h3 class="name">{{ project.name }}</h3>
 
       </RouterLink> <!--set unique identifier key for each project passed over - enabling vue to keep track of variables-->
   </div>
@@ -62,7 +66,13 @@
 </template>
 
 <style scoped>
-.container {
+
+body {
+  --image-width: 30vw;
+  --image-height: calc(var(--image-width)*(2/3));
+  --grid-height: calc(var(--image-height) + 2vw);
+}
+.grid_container {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -76,32 +86,64 @@
   margin-bottom: 200px;
 }
 
-.grid_item:hover {
+.grid_item {
+  height: var(--grid-height);
+  transition: 2s;
+  text-decoration: none;
+}
+
+.image_border {
+  filter: none;
+  transition: 2s;
+}
+
+.image_border:hover {
   filter: drop-shadow(0 0rem 1rem blue);
+  transition: 2s;
+}
+
+.image_container {
+  filter: saturate(70%);
+  overflow: hidden;
+  transition: 1s;
+  height: var(--image-height);
+}
+
+.image_container:hover {
   filter: saturate(100%);
   transition: 2s;
 }
 
-.grid_item {
-  filter: saturate(70%);
-  overflow: hidden;
-  transition: 1s;
-  height: 18vw;
-}
-
 .content_img {
-  width: 30vw;
+  width: var(--image-width);
   transform: scale(1);
   transition: 1s;
 }
 
 .content_img:hover {
-  transform: scale(1.2);
+  transform: scale(1.4);
   transform-origin: v-bind(offsetX + "% ") v-bind(offsetY + "% ");
   transition: transform 1s;
 }
 
+.name {
+  margin-top: 5px;
+  opacity: 0;
+  transition: 0.8s;
+  font-size: medium;
+}
+
+.grid_item:hover .name {
+  opacity: 1;
+  transition: 1s;
+}
+
 @media (max-width: 900px) {
+  body {
+  --m-image-width: calc((30vw*2.33));
+  --m-image-height: calc(var(--m-image-width)*0.66);
+  --m-grid-height: calc((var(--m-image-height) + 2vw));
+  }
 .container {
   display: flex;
   justify-content: center;
@@ -110,19 +152,31 @@
 .projects{
   display: grid;
   grid-template-columns: 1fr;
-  gap: 40px;
-  row-gap: 25px;
+  row-gap: 60px;
   justify-items: center;
   margin-bottom: 200px;
-  align-items: end;
+}
+
+.image_container {
+  height: var(--m-image-height);
 }
 .grid_item {
   filter: saturate(100%);
-  height: 40vw;
+  height: var(--m-grid-height);
 }
 .content_img {
-  width: 70vw;
+  width: var(--m-image-width);
 }
+
+.image_border {
+  filter: drop-shadow(0 0rem 0.6rem blue);
+}
+
+.name {
+  margin-top: 10px;
+  opacity: 1;
+}
+
 
 .content_img:hover {
   transform: scale(1);
