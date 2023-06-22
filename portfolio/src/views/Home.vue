929b3923e76446e7e536b1e8cb595d95325ff93a
david-rollinson <davrc.me@gmail.com>
2023-06-22 15:47:20 +0100

    added note

--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -5,7 +5,7 @@
   <body>
     <div class="intro_text">
     <h3>
-      <!--<p><span style="font-weight: 600; font-size: medium;" >† Please note that this website is currently undergoing construction and content is still being updated. For full details, see <a href="https://droll002.notion.site/droll002/David-Rollinson-Compiled-Info-077934589fbe40f79015748231abe307" target="_blank" rel="noreferrer noopener">my previous site.</a></span></p>-->
+      <p><span style="font-weight: 600; font-size: medium;" >† Please note that this website is currently undergoing construction and content is still being updated. For older projects, see <a href="https://droll002.notion.site/droll002/David-Rollinson-Compiled-Info-077934589fbe40f79015748231abe307" target="_blank" rel="noreferrer noopener">my previous site.</a></span></p>
       <span style="font-weight: 600;" >David Rollinson</span> is a creative technologist who vitalises spaces and materials with interactive technologies. At the centre of their work in experiential environment design and development are computational systems used to investigate phenomenology and social valuation. Their creative skills readily to hand include experience design, 2D/3D graphical programming, CGI design and animation, custom electronics and creative ML. Last year they combined these into a hypermedia installation exhibited at Bargehouse Gallery, Southbank. 
       <p><span style ="font-weight: 600;" >Recently</span> they were a resident creative technologist at <a href="https://thebeamslondon.com/thin-air/" target="_blank" rel="noreferrer noopener">Thin Air</a>, where they also assisted in the development of 'Cyclops Retina, Light Barrier 2.4' by Kimchi and Chips with Rosa Menkman.</p></h3>
     </div>
