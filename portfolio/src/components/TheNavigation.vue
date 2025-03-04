072bd6f62cd55402b4485a5a535382cd6a6ba46f
david-rollinson <davrc.me@gmail.com>
2025-03-04 00:57:03 +0000

    updated nav styling

--- portfolio/src/components/TheNavigation.vue
+++ portfolio/src/components/TheNavigation.vue
@@ -3,7 +3,7 @@
   <nav onclick>
     <!-- 'style:' tag to account for paragraph indent -->
     <RouterLink to="/">David <br>Rollinson</RouterLink>
-    <RouterLink to="/work">Selected <br>Projects</RouterLink>
+    <RouterLink to="/work" :class="{ 'work-link': $route.path.startsWith('/work') }">Selected <br>Projects</RouterLink>
     <RouterLink to="/info">Info</RouterLink>
   </nav>
 </div>
